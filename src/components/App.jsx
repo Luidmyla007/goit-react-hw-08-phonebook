c636fee91d2008561dbb226a9a2db908b1520d1d
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-12 13:21:53 +0300

    Revert "add 07 hw"
    This reverts commit e39e6c01e2c2a82cc608b370517d2c073c2f55cf.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,16 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
-
-import { GlobalStyle } from './GlobalStyles';
-import { Container } from './App.styled';
-import { ContactStyled } from './App.styled';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-
-export const App = () => { 
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  
-     return (
-     <Container>
-         <GlobalStyle/>        
-        <ContactForm />        
-        <ContactStyled>
-          <h2>Contacts</h2>
-          <Filter/>
-          {isLoading && !error && <b>Request in progress...</b>}
-          <ContactList/>          
-        </ContactStyled>
-     </Container>
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/components/Filter/Filter.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const FilterStyled = styled.div`
-  padding-top: 10px; 
-  margin - bottom: 25px;
-`;
-
