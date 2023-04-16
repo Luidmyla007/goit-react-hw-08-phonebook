c831722bfd7e36baf6e65608307a68212e8462ed
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-16 21:25:54 +0300

    add modern-normalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+
 import { Layout } from './Layout';
 import { refreshUser } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
 import PrivateRoute from './PrivateRoute';
 import RestrictedRoute from './RestrictedRoute';
+import 'modern-normalize';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Register = lazy(() => import('../pages/Register/Register'));
