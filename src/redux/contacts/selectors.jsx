cc3b5aec03783c54487b66a226284946b2d96da0
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-19 11:22:51 +0300

    fixed filter

--- src/redux/contacts/selectors.jsx
+++ src/redux/contacts/selectors.jsx
@@ -1,4 +1,3 @@
-import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.items;
 
@@ -6,18 +5,5 @@ export const selectIsLoading = state => state.contacts.isLoading;
 
 export const selectError = state => state.contacts.error;
 
-export const selectFilter = state => state.filter.value;
-
 export const selectIsContactsEmpty = state => selectContacts(state).length === 0;
 
-export const selectFilterValue = createSelector(
-  [selectContacts, selectFilter],
-    (contacts, filter) => {
-      if (!filter) {
-        return contacts.items;
-    }
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
-);
