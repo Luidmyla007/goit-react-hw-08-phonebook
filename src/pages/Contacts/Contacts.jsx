90b750964f0d1fd42c2eadcf965db02bc8a9bd11
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-19 12:21:44 +0300

    fixed contactForm

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -3,27 +3,23 @@ import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectIsContactsEmpty, selectError } from 'redux/contacts/selectors';
+import { selectIsContactsEmpty } from 'redux/contacts/selectors';
 import { fetchContacts } from 'redux/contacts/operations';
+// import { Helmet } from 'react-helmet';
 
 
 const Contacts = () => {
   const dispatch = useDispatch();
   const isContactsEmpty = useSelector(selectIsContactsEmpty);
-  const Error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
-  if (!Error) {
-    return alert(`Oops, something went wrong`);
-  }
-
   return (
-    <div className="wrapper">     
-      <ContactForm />
-      <h2>Contacts</h2>
+    <div>     
+      <ContactForm />   
+      <h2>Contacts</h2>  
       <Filter />
       {isContactsEmpty ? (        
         <h2>Contactbook is empty, please add your first contact!</h2>
