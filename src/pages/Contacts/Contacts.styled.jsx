b9beb3ebc77ca7eccdc7bc19b4075f8a5b986594
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-19 15:34:26 +0300

    add styles for ContactPage

---
+++ src/pages/Contacts/Contacts.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: calc(100vh - 50px);
+  flex-direction: column;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
